4d93aeab627c86228478b55dc9c585f372b0c38c
Paweł Batko <pbatko@virtuslab.com>
2015-11-04 15:39:53 +0100

    Upgrade to Unicorn 1.0.0, Slick 3.1.0 and Play 2.4.3

--- app/repositories/UserRepository.scala
+++ app/repositories/UserRepository.scala
@@ -1,9 +1,9 @@
 package repositories
 
 import org.virtuslab.unicorn.LongUnicornPlay._
-import org.virtuslab.unicorn.LongUnicornPlay.driver.simple._
+import org.virtuslab.unicorn.LongUnicornPlay.driver.api._
 import model.{UserRow, Users, UserId}
-import scala.slick.lifted.TableQuery
+import slick.lifted.TableQuery
 
 class UserRepository(query: TableQuery[Users])
   extends BaseIdRepository[UserId, UserRow, Users](query)
